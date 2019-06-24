e572b5fa33284620ebcf64683539de817704b90a
EugenTepin <4godwokr@gmail.com>
2019-06-24 18:16:30 +0400

    Draft of main methods of Todo app class

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function TodoItem(status, text) {
+function TodoItem(text, status) {
     this.status = (status !== void 0) ? !!(status) : false;
     this.text = text || "";
     this.id = (new Date()).getTime();
@@ -18,48 +18,66 @@ function TodoList() {
 }
 
 TodoList.prototype.init = function (store, render) {
-    //this.store = store;
-    //this.output = output;
-    //this.stateId = ;
-    // init(strJSON, this) 
-    //this.render = render;
+    this.store = store;
+    this.render = render;
+    var that = this;
+    var store = this.store;
+    var stateStr = store.getItem('Todo state');
+    if (stateStr !== null) {
+        var state = JSON.parse(stateStr);
+        Object.keys(state).forEach(function (prop) {
+            that[prop] = state[prop];
+        });
+        // this.items = state.items;
+        // this.pageLimit = this.pageLimit;
+        // this.currentPage = this.currentPage;
+        // this.currentFilter = this.currentFilter;
+    }
     this.render();
 };
 
-// TodoList.prototype.prevState = function () {
-//     // getPrevStateId(store,stateID)
-//     // init(strJSON, this) 
-//     this.render();
-// };
-
-// TodoList.prototype.nextState = function () {
-//     // getNextStateId(store,stateID)
-//     // init(strJSON, this) 
-//     this.render();
-// };
-
 TodoList.prototype.saveToStore = function () {
-
-};
-
-TodoList.prototype.render = function () {
-
+    if (this.store === void 0) {
+        throw new Error("Store is undefined!");
+    }
+    var store = this.store;
+    var app = Object.assign({}, this);
+    delete app.store;
+    delete app.render;
+    store.setItem('Todo state', JSON.stringify(app));
 };
 
 TodoList.prototype.addItem = function (text) {
+    var item = new TodoItem(text);
+    this.items.push(item);
     this.saveToStore();
     this.render();
 };
 TodoList.prototype.deleteItem = function (id) {
-    this.saveToStore();
-    this.render();
+    var index;
+    var deleteFlag = this.items.some(function (item, i) {
+        index = i;
+        return item.id === id;
+    });
+    if (deleteFlag) {
+        this.items.splice(index, 1);
+        this.saveToStore();
+        this.render();
+    }
 };
-TodoList.prototype.filter = function (filter) {
-    this.saveToStore();
-    this.render();
-};
-TodoList.prototype.changePage = function (page) {
+
+TodoList.prototype.changeFilter = function (filter) {
+    this.currentFilter = filter;
     this.saveToStore();
     this.render();
 };
 
+TodoList.prototype.changePage = function (page) {
+    this.currentPage = page;
+    this.saveToStore();
+    this.render();
+};
+
+function renderConsole() {
+
+}
